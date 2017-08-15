84539304eedc82ee03fea57db06c5b03169ea60c
Emilia Pavese <emeleaaa@gmail.com>
2017-08-15 18:26:23 -0300

    Agrego ejercicios JS

---
+++ JS/ej36.js
@@ -0,0 +1,4 @@
+let numero1 = 10;
+let numero2 = 20;
+console.log(numero1 == numero2);
+console.log(numero1 === numero2);
---
+++ JS/ej37.js
@@ -0,0 +1,6 @@
+let promedioParaAprobar = 6;
+let promedioDelAlumno = 4;
+let resultado = (promedioDelAlumno >=6)
+let resultado2 = (promedioParaAprobar - promedioDelAlumno)
+console.log('El alumno tiene un promedio mayor o igual que el requerido: ' + resultado);
+console.log(`Al alumno le faltaron ${resultado2} puntos para aprobar`);
